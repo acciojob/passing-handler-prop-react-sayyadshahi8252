b64ad816e310e388c281b83ad939abe368e45229
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-11-13 11:50:55 +0530

    Add Selection component with color application

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
 
+import React, { useState } from 'react';
+
+const Selection = ({ applyColor }) => {
+  const [selectionStyle, updateSelectionStyle] = useState({ background: "" });
+
+  return (
+    <div
+      className="fix-box"
+      style={selectionStyle}
+      onClick={() => applyColor(updateSelectionStyle)}
+    >
+    </div>
+  );
+};
+
+export default Selection;
